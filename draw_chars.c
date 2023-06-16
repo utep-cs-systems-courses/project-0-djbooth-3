381f77c48d35784cf89b5741a2aa518f578f26f0
djbooth-3 <djbooth@miners.utep.edu>
2023-06-16 14:10:01 -0600

    Updated

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,43 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void print_char_5x7(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 5; row++) {
+    unsigned short rowBits = font_5x7[c][row];
+    for (char col = 0; col < 7; col++) {
+      unsigned short colMask = 1 << (6-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 10; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (11-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
+void print_char_8x12V(char c)
+{
+  c -= 0x20;
+  for (char col = 0; col < 11; col++) {
+    unsigned short rowBits = 1 << (11-col);
+    for (char row = 0; row < 10; row++) {
+      unsigned short colMask = font_8x12[c][row];
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+
--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars(11x16 font) = c, Chars(5x7 font) = d, Chars(8x12 font) = f, Chars(8x12 font Vertical) = g, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -25,9 +25,28 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected chars (11x16 font):");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'd':
+      puts("You selected chars (5x7 font):");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_5x7(c);
+      break;
+    case 'f':
+      puts("You selected chars (8x21 font):");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
+      break;
+    case 'g':
+      puts("You selected char (8x12 font Vertical):");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12V(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5,7);
       break;
     case 'q':
       puts("Bye!");
